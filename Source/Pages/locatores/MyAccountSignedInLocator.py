62336057f6092b2fddf00e077f482fc85f225596
Vignesh Palanisamy <133522715+Vignesh442@users.noreply.github.com>
2024-04-03 13:52:05 +0530

    Create MyAccountSignedInLocator.py

---
+++ Source/Pages/locatores/MyAccountSignedInLocator.py
@@ -0,0 +1,6 @@
+
+from selenium.webdriver.common.by import By
+
+class MyAccountSignedInLocator:
+
+    LEFT_NAV__LOGOUT_BTN=(By.CSS_SELECTOR,'#post-9 > div > div > nav > ul > li.woocommerce-MyAccount-navigation-link.woocommerce-MyAccount-navigation-link--customer-logout > a')
